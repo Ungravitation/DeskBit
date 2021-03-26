4bf22903a1d80db57566aaf5757a59b397bfd85d
Ungravitation <52806340+Ungravitation@users.noreply.github.com>
2021-03-26 08:57:20 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -218,23 +218,23 @@ namespace DeskBit {
     function valuotokeyConversion():number{
         let irdata:number;
         switch(irCode()){
-            case 0xff629D:irdata = 12;break;
-            case 0xff22DD:irdata = 13;break;
-            case 0xffA857:irdata = 14;break;
-            case 0xffC23D:irdata = 15;break;
-            case 0xff02FD:irdata = 16;break;
-            case 0xff4AB5:irdata = 0;break;
-            case 0xff6897:irdata = 1;break;
-            case 0xff9867:irdata = 2;break;
-            case 0xffB04F:irdata = 3;break;
-            case 0xff30CF:irdata = 4;break;
-            case 0xff18E7:irdata = 5;break;
-            case 0xff7A85:irdata = 6;break;
-            case 0xff10EF:irdata = 7;break;
-            case 0xff38C7:irdata = 8;break;
-            case 0xff5AA5:irdata = 9;break;
-            case 0xff42BD:irdata = 10;break;
-            case 0xff52AD:irdata = 11;break;
+            case 0xf629D:irdata = 12;break;
+            case 0xf22DD:irdata = 13;break;
+            case 0xfA857:irdata = 14;break;
+            case 0xfC23D:irdata = 15;break;
+            case 0xf02FD:irdata = 16;break;
+            case 0xf4AB5:irdata = 0;break;
+            case 0xf6897:irdata = 1;break;
+            case 0xf9867:irdata = 2;break;
+            case 0xfB04F:irdata = 3;break;
+            case 0xf30CF:irdata = 4;break;
+            case 0xf18E7:irdata = 5;break;
+            case 0xf7A85:irdata = 6;break;
+            case 0xf10EF:irdata = 7;break;
+            case 0xf38C7:irdata = 8;break;
+            case 0xf5AA5:irdata = 9;break;
+            case 0xf42BD:irdata = 10;break;
+            case 0xf52AD:irdata = 11;break;
             default:
             irdata = -1;
         }
